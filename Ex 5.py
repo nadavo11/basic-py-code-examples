48008078871fbcc9914d1fdd431ee738789f509e
nadavo11 <nadavo11@gmail.com>
2022-11-02 19:22:06 +0200

    exercises 1 - 4 are now ready! 🥇🥇🥇🥇

--- Ex 5.py
+++ Ex 5.py
@@ -9,7 +9,7 @@ passwords - strong passwords have a mix of lowercase letters, uppercase
 letters, numbers, and symbols.
 The passwords should be random, generating a new password every time the
 user asks for a new password.
-Include your run-time code in a main method
+Include your run-time code in a main method1
 
 _____________________________________________________________________________
 """
